366fdab725b396ad0da6e23e883deee3f98cfdc0
David <olayinkadavid62@yahoo.com>
2022-04-26 14:46:25 +0100

    Update

--- src/components/Educationalexp.js
+++ src/components/Educationalexp.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import './style.css'
 
-function educationalexperience() {
+function Educationalexp() {
   return (
     <div class="row2">
       <h4 class="educationalexp">Educational Experience</h4>
@@ -8,10 +9,10 @@ function educationalexperience() {
       <div class="details2">
         <p>School: Obafemi Awolowo University</p>
         <p>Title of study: Civil Engineering</p>
-        <p>Date of study: 2015-202</p>
+        <p>Date of study: 2015-2021</p>
       </div>
     </div>
   )
 }
 
-export default educationalexperience
+export default Educationalexp
